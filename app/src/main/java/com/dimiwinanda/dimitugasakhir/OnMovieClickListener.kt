7b19acad598f327c2329432a33a4a19ddb0eb10e
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-06-15 03:36:05 +0700

    Done1

--- app/src/main/java/com/dimiwinanda/dimitugasakhir/OnMovieClickListener.kt
+++ app/src/main/java/com/dimiwinanda/dimitugasakhir/OnMovieClickListener.kt
@@ -1,5 +1,7 @@
+//untuk mendeklarasikan package com.dimiwinanda.dimitugasakhir kita dapat menggunakan kata kunci package.
 package com.dimiwinanda.dimitugasakhir
 
+//Metode OnMovieClickListener ini dipanggil bila pengguna menyentuh item.
 interface OnMovieClickListener {
 
     fun onMovieItemClicked(position : Int)
