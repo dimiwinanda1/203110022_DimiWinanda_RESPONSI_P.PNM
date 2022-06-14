382d18160ca835df65f718a1b002ac8431f61f5d
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-06-15 03:22:04 +0700

    Done1

--- app/src/main/java/com/dimiwinanda/dimitugasakhir/MovieViewHolder.kt
+++ app/src/main/java/com/dimiwinanda/dimitugasakhir/MovieViewHolder.kt
@@ -1,8 +1,12 @@
+//untuk mendeklarasikan package com.dimiwinanda.dimitugasakhir kita dapat menggunakan kata kunci package.
 package com.dimiwinanda.dimitugasakhir
 
+//kita dapat menggunakan deklarasi import untuk mengaktifkan compiler untuk menemukan class, function, interface atau object yang akan diimpor.
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class MovieViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+//ViewHolder adalah wrapper di sekitar View yang berisi tata letak untuk setiap item dalam daftar.
+//itemView ini adalah method yang disediakan class ViewHolder untuk mengambil root ViewGroup.
+class MovieViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {c
 
 }
