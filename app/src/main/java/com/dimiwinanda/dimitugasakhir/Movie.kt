5deceb5441d4569c120aa3abb56500a682b174de
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-06-14 22:05:06 +0700

    Done1

--- app/src/main/java/com/dimiwinanda/dimitugasakhir/Movie.kt
+++ app/src/main/java/com/dimiwinanda/dimitugasakhir/Movie.kt
@@ -1,3 +1,6 @@
+//untuk mendeklarasikan package com.dimiwinanda.dimitugasakhir kita dapat menggunakan kata kunci package.
 package com.dimiwinanda.dimitugasakhir
 
+//script dibawah merupakan model untuk sebuah data movie
+//dimana val adalah variabel yang tidak dapat diubah dan hanya dapat diinternalisasi satu kali saja.
 data class Movie(val name:String, val genre:String, val year:String)
